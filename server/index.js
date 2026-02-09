d7caa74347a02a335c8df9de52f3620facd90ef6
Eduard Bozgan <eduard.kati@gmail.com>
2026-02-09 17:19:03 +0200

    server: changed listening address to default gateway

--- server/index.js
+++ server/index.js
@@ -36,4 +36,6 @@ io.on("connection", (socket) => {
 app.get("/healthz", (_req, res) => res.json({ ok: true }));
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Server listening on http://localhost:${PORT}`));
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server listening on http://0.0.0.0:${PORT}`);
+});
